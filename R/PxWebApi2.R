8afee7da31b11599f5c656d0d0dc87df8123778b
Langsrud <oyl@ssb.no>
2025-12-15 08:42:03 +0100

    Add meta_data() to retrieve PxWebApi 2 metadata

--- R/PxWebApi2.R
+++ R/PxWebApi2.R
@@ -1,4 +1,49 @@
 
+
+
+
+#' PxWebApi 2 metadata for a table
+#'
+#' Retrieves metadata for a table using the PxWebApi 2 endpoint and returns it
+#' as an R object via [jsonlite::read_json()].
+#'
+#' @inheritParams make_url
+#' @param url_or_tableid Either a numeric table id or a PxWebApi 2 URL to data.
+#'   When a data URL is supplied, it is internally converted to a metadata URL.
+#'
+#' @returns
+#' A list containing table metadata as returned by [jsonlite::read_json()].
+#'
+#' @export
+#'
+#' @examples
+#' metadata1 <- meta_data(8991, url_type = "ssb_en")
+#' metadata2 <- meta_data(
+#'   "https://statistikdatabasen.scb.se/api/v2/tables/TAB1525/data?lang=en"
+#' )
+#'
+#' print(metadata1[1:4])
+#' print(metadata2[1:4])
+meta_data <- function(url_or_tableid,  url_type = "ssb") {
+  Graceful(meta_data_, url_or_tableid, url_type)
+}
+
+meta_data_ <- function(url_or_tableid,  url_type) {
+  url <- ensure_url(url_or_tableid, url_type)
+  url <- metadata_url(url)  
+  jsonlite::read_json(url)
+}
+
+
+ensure_url <- function(url_or_tableid,  url_type) {
+  integer_url <- suppressWarnings(as.integer(url_or_tableid))  
+  if (!is.na(integer_url)) {
+    url_or_tableid <- make_url(integer_url, url_type = url_type) 
+  } 
+  url_or_tableid
+}
+
+
 #' Make data url from table id
 #'
 #' @param id Table id (integer).
